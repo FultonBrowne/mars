81e21151f36436a7aa7c98192f0c64204ead8c01
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-30 10:07:38 -0700

    Create Alert.kt

---
+++ sdk/src/main/kotlin/com/andromeda/mars/AraMain/Alert.kt
@@ -0,0 +1,10 @@
+package com.andromeda.mars.AraMain
+
+class Alert {
+    val ANDROID_DEVICE = 1
+    val DESKTOP_DEVICE = 2
+    val SERVER_DEVICE = 3
+    fun Alert(type:Int, title: String, info: String, image: String,link: String, bubble: Boolean){
+
+    }
+}
