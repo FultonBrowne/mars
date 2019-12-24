f7540b1cb18abf7a049a655abcd046a3979e2927
fulton <gprlabs@outlook.com>
2019-12-24 11:45:36 -0800

    adding files for nlp

---
+++ src/main/java/com/andromeda/mars/nlp/MarsNLP.kt
@@ -0,0 +1,13 @@
+package com.andromeda.mars.nlp
+/**
+ * The main class for NLP
+ * Tip just change the sentence value Its faster than making new instances every time.
+ * **/
+class MarsNLP {
+    /**
+     * the value to be parser
+     * **/
+    var sentence:String? = null
+    fun getNouns(){}
+
+}
