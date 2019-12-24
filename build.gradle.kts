f133cfe461f883a8023fc290009f33afe867e351
fulton <gprlabs@outlook.com>
2019-12-24 11:21:48 -0800

    adding files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,29 @@
-group = "org.example"
-version = "1.0-SNAPSHOT"
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+version = "1.2.1"
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+    mavenCentral()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
---
+++ src/main/java/com/andromeda/mars/Demo.kt
@@ -0,0 +1,4 @@
+package com.andromeda.mars
+class Demo {
+
+}
