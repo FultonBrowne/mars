b14a81172cf85ddcf167844115de4b88488dd1d0
fulton <gprlabs@outlook.com>
2019-12-24 16:39:38 -0800

    adding files for nlp

--- src/main/java/com/andromeda/mars/nlp/MarsNLP.kt
+++ src/main/java/com/andromeda/mars/nlp/MarsNLP.kt
@@ -1,13 +1,22 @@
 package com.andromeda.mars.nlp
+
+import opennlp.tools.parser.Parser
+import opennlp.tools.parser.ParserFactory
+import opennlp.tools.parser.ParserModel
+import java.io.InputStream
+
 /**
  * The main class for NLP
  * Tip just change the sentence value Its faster than making new instances every time.
  * **/
-class MarsNLP {
+class MarsNLP(model:InputStream) {
     /**
-     * the value to be parser
+     * the value to be parsed
      * **/
+    private var parse: Parser = ParserFactory.create(ParserModel(model))
     var sentence:String? = null
-    fun getNouns(){}
+    fun getNouns(){
+
+    }
 
 }
