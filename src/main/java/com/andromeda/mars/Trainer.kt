02423ff1326bc9cddb1debc71f51730ada27c6af
fulton <gprlabs@outlook.com>
2020-01-08 17:35:49 -0800

    awesome thing coming...

--- src/main/java/com/andromeda/mars/Trainer.kt
+++ src/main/java/com/andromeda/mars/Trainer.kt
@@ -2,11 +2,17 @@ package com.andromeda.mars
 
 import java.io.InputStream
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class Trainer {
     fun main(data:InputStream){
         val s: Scanner = Scanner(data).useDelimiter("\\A")
         val fullText = if (s.hasNext()) s.next() else ""
+        val list = JsonParse().preTrainer(fullText)
+        val finalList = ArrayList<TrainedData>()
+        for (i in list){
+
+        }
     }
 }
