1172eee2d2a777318ada52d1b7d3ee7bbd876de1
fulton <gprlabs@outlook.com>
2020-01-09 11:28:20 -0800

    awesome thing coming...

--- src/main/java/com/andromeda/mars/Trainer.kt
+++ src/main/java/com/andromeda/mars/Trainer.kt
@@ -1,5 +1,6 @@
 package com.andromeda.mars
 
+import java.io.FileInputStream
 import java.io.InputStream
 import java.util.*
 import kotlin.collections.ArrayList
@@ -12,7 +13,9 @@ class Trainer {
         val list = JsonParse().preTrainer(fullText)
         val finalList = ArrayList<TrainedData>()
         for (i in list){
-
+            val file = Trainer::class.java.getResource(i.dataDir)
+            val dataToMerge = file.content
+            println(dataToMerge)
         }
     }
 }
