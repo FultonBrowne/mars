68c4a2cde52043c45e69cbff288e722e7466c99f
fulton <gprlabs@outlook.com>
2020-01-21 15:06:25 -0800

    awesome thing coming...

--- src/main/java/com/andromeda/mars/brain/Brain.kt
+++ src/main/java/com/andromeda/mars/brain/Brain.kt
@@ -2,9 +2,17 @@ package com.andromeda.mars.brain
 
 class Brain {
     fun main(){
+        val i = 1
         val threads = arrayListOf<BrainType>()
+        threads.add(BrainType(Thread{inputCell()}, 0))
+        while (i < 5){
+
+        }
 
     }
     fun brainTransistor(threads: ArrayList<BrainType>, cellInt:Int){
+    }
+    fun inputCell(){
+
     }
 }
