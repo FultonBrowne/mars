6fd21b2c08a5a8018e3a317b5394ae1822380e2c
fulton <gprlabs@outlook.com>
2020-01-22 20:06:23 -0800

    awesome thing coming...

--- src/main/java/com/andromeda/mars/brain/Cell.kt
+++ src/main/java/com/andromeda/mars/brain/Cell.kt
@@ -1,24 +1,9 @@
 package com.andromeda.mars.brain
 
 class Cell(index:Int, size:Int, unitWidth:Int): CellBase(index, unitWidth) {
-    private val i = index
-    val s = size
-    val u = unitWidth
     init {
-        val index = i - unitWidth / 2
         if (index < 0){
 
-        }
-    }
-    override fun getData(newData:Int){
-        data.add(newData)
-        if (data.size == u){
-            var finalData = 0
-            data.forEach {
-                if (finalData == 0)finalData = it
-                else finalData.xor(it)
-            }
-            data.clear()
         }
     }
 
