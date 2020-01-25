0ee0ddf616c5970251b0f4c9721c82771e585e0a
fulton <gprlabs@outlook.com>
2020-01-25 12:01:16 -0800

    awesome thing coming...

---
+++ src/main/java/com/andromeda/mars/brain/CLI.kt
@@ -0,0 +1,11 @@
+package com.andromeda.mars.brain
+
+class CLI {
+    fun main(){
+        println("how big is the data set?")
+        val size = readLine()!!.toInt()
+        println("communication width?")
+        val inSize = readLine()!!.toInt()
+
+    }
+}
