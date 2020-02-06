d0dc30455aec209de5a76612338c82cb241cacf9
fulton <gprlabs@outlook.com>
2020-02-05 16:27:38 -0800

    working on tsv reader

--- src/main/java/com/andromeda/mars/readers/TSV.kt
+++ src/main/java/com/andromeda/mars/readers/TSV.kt
@@ -1,4 +1,14 @@
 package com.andromeda.mars.readers
 
+import com.univocity.parsers.tsv.TsvParser
+import com.univocity.parsers.tsv.TsvParserSettings
+
 class TSV {
+    fun main(path:String){
+        val settings = TsvParserSettings()
+        settings.getFormat().setLineSeparator("\n")
+        val parser = TsvParser(settings)
+        val allRows = parser.parseAllRecords(GetReader().getReader(path))
+        for(i in allRows){}
+    }
 }
