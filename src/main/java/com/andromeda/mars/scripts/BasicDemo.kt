1e576441625b03a8baeb994861c49e4b311628d5
fulton <gprlabs@outlook.com>
2020-02-14 13:38:54 -0800

    first basic test works

--- src/main/java/com/andromeda/mars/scripts/BasicDemo.kt
+++ src/main/java/com/andromeda/mars/scripts/BasicDemo.kt
@@ -6,6 +6,7 @@ class BasicDemo {
     fun main(){
         val cell = Generate().amlModel( 4)
         cell.dataToCell(arrayListOf(0, 0, 2, 0), 0)
+        cell.dataToCell(arrayListOf(5, 2, 2, 0), 3)
         val recognize = cell.recognize(arrayListOf(0, 0, 2, 0))
         println(recognize)
     }
