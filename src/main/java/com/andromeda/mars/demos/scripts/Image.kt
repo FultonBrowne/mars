662189aac78cc37f51b67afd82ea0c1f67da4562
fulton <gprlabs@outlook.com>
2020-02-17 12:35:50 -0800

    working on image reading

--- src/main/java/com/andromeda/mars/demos/scripts/Image.kt
+++ src/main/java/com/andromeda/mars/demos/scripts/Image.kt
@@ -1,7 +1,24 @@
 package com.andromeda.mars.demos.scripts
 
+import com.andromeda.mars.brain.CellBase
+import java.awt.Label
+import java.io.File
+import java.io.FileInputStream
+import java.io.ObjectInputStream
+
+
 class Image {
+    //read the cifar-10 dataset
     fun main(){
+        val cell = CellBase()
+        val file = File("CharacterStats.dat")
+        val fis = FileInputStream(file)
+        val airplanesByteArray = fis.readAllBytes()
+    }
+
+    fun writeData(label: String, data:Any, cellBase: CellBase){
 
     }
+
+
 }
