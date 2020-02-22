8fae6b90960dc06aa2f669faa7465cbfeb9db447
fulton <gprlabs@outlook.com>
2020-02-21 19:06:04 -0800

    test done for main

---
+++ src/test/java/com/andromeda/mars/test/MainTest.kt
@@ -0,0 +1,19 @@
+package com.andromeda.mars.test
+
+import com.andromeda.mars.brain.CellBase
+import org.junit.Assert
+import org.junit.Test
+
+class MainTest {
+    @Test
+    fun test(){
+        val cellBase = CellBase()
+        val data1 = arrayListOf(0, 0, 0, 2)
+        cellBase.dataToCell(data1, 2)
+        Assert.assertEquals(2, cellBase.recognize(data1))
+        cellBase.dataToCell(data1, 2)
+        Assert.assertEquals(2, cellBase.recognize(data1))
+
+        cellBase.freeze()
+    }
+}
